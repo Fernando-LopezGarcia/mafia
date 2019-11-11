dfda1ea9bf693ef8bb275081afc2e118c4cfe1d6
Fernando Lopez <fernando@Fernandos-MacBook-Pro.local>
2019-11-11 04:11:54 -0800

    added /build for deployment

---
+++ front-end/build/precache-manifest.456203205761efd283ba011453564e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407d0235f3b144edcc813e9793d97073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79064e59b8a3f417da3f",
+    "url": "/static/css/main.e18fbca8.chunk.css"
+  },
+  {
+    "revision": "aabe292e64e304eb0d4e",
+    "url": "/static/js/2.2f64511d.chunk.js"
+  },
+  {
+    "revision": "79064e59b8a3f417da3f",
+    "url": "/static/js/main.f94dc7cd.chunk.js"
+  },
+  {
+    "revision": "abe1baa7ce5ff05bfcc3",
+    "url": "/static/js/runtime-main.1870464d.js"
+  },
+  {
+    "revision": "d7f3f346fe5f464fb8770b25a4ef4fd4",
+    "url": "/static/media/mafia.d7f3f346.png"
+  }
+]);
