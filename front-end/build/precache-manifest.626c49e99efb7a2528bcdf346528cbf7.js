1f0b7fc0cbe3fcfb105e0cd67aa1c7f467862364
Fernando Lopez <fernando@Fernandos-MacBook-Pro.local>
2019-11-11 21:59:53 -0800

    added static folder

---
+++ front-end/build/precache-manifest.626c49e99efb7a2528bcdf346528cbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1369c07b1724e9fcbf05d9842d4561b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ade8e735cc8073d3037",
+    "url": "/static/css/main.e18fbca8.chunk.css"
+  },
+  {
+    "revision": "95293e49188352b2a932",
+    "url": "/static/js/2.d4264f5f.chunk.js"
+  },
+  {
+    "revision": "4ade8e735cc8073d3037",
+    "url": "/static/js/main.710a9eda.chunk.js"
+  },
+  {
+    "revision": "abe1baa7ce5ff05bfcc3",
+    "url": "/static/js/runtime-main.1870464d.js"
+  },
+  {
+    "revision": "d7f3f346fe5f464fb8770b25a4ef4fd4",
+    "url": "/static/media/mafia.d7f3f346.png"
+  }
+]);
